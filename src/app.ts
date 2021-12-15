9e61eb288550acaf945acdc401579336e264cc71
Sai Sridhar Akula <sridhar.akula001@gmail.com>
2021-12-15 14:28:36 +0530

    Using a getter

--- src/app.ts
+++ src/app.ts
@@ -161,6 +161,31 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 	protected abstract renderContent(): void;
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+	get persons() {
+		if (this.project.people === 1) {
+			return "1 person";
+		}
+		return this.project.people + " persons";
+	}
+
+	constructor(hostId: string, private project: Project) {
+		super("single-project", hostId, false, project.id);
+
+		this.configure();
+		this.renderContent();
+	}
+
+	protected configure() {}
+	protected renderContent() {
+		this.element.querySelector("h2")!.textContent = this.project.title;
+		this.element.querySelector("h3")!.textContent =
+			this.persons + " assigned";
+		this.element.querySelector("p")!.textContent = this.project.description;
+	}
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 	assignedProjects: Project[] = [];
@@ -191,9 +216,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 		)! as HTMLUListElement;
 		listEl.innerHTML = "";
 		for (const prjItem of this.assignedProjects) {
-			const listItem = document.createElement("li");
-			listItem.textContent = prjItem.title;
-			listEl.appendChild(listItem);
+			new ProjectItem(this.element.querySelector("ul")!.id, prjItem);
 		}
 	}
 
@@ -204,9 +227,6 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 			this.type.toUpperCase() + " PROJECTS";
 	}
 }
-
-// ProjectItem Class
-class ProjectItem {}
 
 // ProjectInput Class
 class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
